3eccf3e8daa1c3d23563c59ea1406a702b882ec8
David Simon <davidsimon@gmail.com>
2025-05-17 13:28:47 +0900

    add absolute path of manage page

--- manage_page/dist/assets/ref-D0P0v7LF.js
+++
@@ -1,5 +0,0 @@
-import{a5 as o}from"./index-CquxMrby.js";import{a as n,f as c}from"./if-defined-BNoSNjAJ.js";/**
- * @license
- * Copyright 2020 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */const d=()=>new r;class r{}const e=new WeakMap,G=n(class extends c{render(t){return o}update(t,[s]){var h;const i=s!==this.G;return i&&this.G!==void 0&&this.rt(void 0),(i||this.lt!==this.ct)&&(this.G=s,this.ht=(h=t.options)==null?void 0:h.host,this.rt(this.ct=t.element)),o}rt(t){if(this.isConnected||(t=void 0),typeof this.G=="function"){const s=this.ht??globalThis;let i=e.get(s);i===void 0&&(i=new WeakMap,e.set(s,i)),i.get(this.G)!==void 0&&this.G.call(this.ht,void 0),i.set(this.G,t),t!==void 0&&this.G.call(this.ht,t)}else this.G.value=t}get lt(){var t,s;return typeof this.G=="function"?(t=e.get(this.ht??globalThis))==null?void 0:t.get(this.G):(s=this.G)==null?void 0:s.value}disconnected(){this.lt===this.ct&&this.rt(void 0)}reconnected(){this.rt(this.ct)}});export{d as e,G as n};
--- manage_page/vite.config.mjs
+++ manage_page/vite.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: './',
   plugins: [
     react(), 
     tailwindcss()
