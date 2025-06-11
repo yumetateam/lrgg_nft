2d2eaf8f0e606ccf566fd7010aeae08a2dbe7784
David Simon <davidsimon@gmail.com>
2025-06-11 20:07:47 +0900

    change metadata image url

--- scripts/create_metadata.py
+++ scripts/create_metadata.py
@@ -6,7 +6,7 @@ output_dir = os.path.join(os.path.dirname(__file__), '../metadata')
 os.makedirs(output_dir, exist_ok=True)  # Create the directory if it doesn't exist
 
 # Base image URL for each NFT
-base_image_url = 'ipfs://bafybeigg6yblnctukey7ntwhgubzyrij4s7ofbq64qfwyqj7i7puweufzu/'
+base_image_url = 'ipfs://bafybeia6eyrbvml536w67avwwkr2fjyeig3p52qqkb5uad3ac5uqmvpdai/'
 
 # Generate metadata for token IDs from 1001 to 1300
 for token_id in range(1, 301):
